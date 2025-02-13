88faca19f1e79d8d251c750e07011175d6b02dc7
Oliveiraar <santos.rodrigooliviera@gmail.com>
2025-02-13 21:06:48 +0000

    Criação de atributos separados e cadastro com printf e scanf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,11 @@
 //Teste larissa
 
 int main() {
+    int cod_cidade, populacao, num_pontos;
+    double area;
+    double pib;
+    char nome [50];
+      
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -17,6 +22,23 @@ int main() {
     // Exibição dos Dados das Cartas:
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+    printf("Digite o código da Cidade: \n");
+    scanf("%d", &cod_cidade);
+
+    printf("Digite o nome da cidade: \n");
+    scanf("%s", &nome);
+
+    printf("Digite o número populacional da cidade: \n");
+    scanf("%d", &populacao);
+
+    printf("Digite a área da cidade: \n");
+    scanf(" %f", &area);
+
+    printf("Digite o PIB da Cidade: \n");
+    scanf("%f", &pib);
+
+    printf("Digite o número de pontos turísticos: \n");
+    scanf("%d", &num_pontos);
 
     return 0;
 }
